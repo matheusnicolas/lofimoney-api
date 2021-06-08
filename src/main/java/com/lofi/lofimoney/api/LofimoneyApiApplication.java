ba382f20708dbf5699d1507f3fcb30d814b41dea
matheusnicolas <matheuspb8@gmail.com>
2021-06-07 21:36:18 -0300

    first commit

---
+++ src/main/java/com/lofi/lofimoney/api/LofimoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.lofi.lofimoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofimoneyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LofimoneyApiApplication.class, args);
+    }
+
+}
